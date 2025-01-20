0a8d9d8fd768ba3fed2c18a8619cc962d80e2b54
hrishikeshtawade04 <hrishikeshtawade04@gmail.com>
2025-01-19 18:10:47 -0800

    Added number systems

---
+++ number_systems.py
@@ -0,0 +1,12 @@
+a = 25
+b = bin(a)
+print(b) # 0b11001
+print(type(b)) # <class 'str'>
+print(b[1]) # b
+
+c = oct(a)
+print(c) # 0o31
+print(type(c)) # <class 'str'>
+d = hex(a)
+print(d) # 0x19
+print(type(d)) # <class 'str'>
