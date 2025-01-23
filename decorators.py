5a8128455644d5b37fdaaf300a55ac4485e78c61
hrishikeshtawade04 <hrishikeshtawade04@gmail.com>
2025-01-22 19:32:27 -0800

    Added decorators

---
+++ decorators.py
@@ -0,0 +1,20 @@
+def div(a, b):
+    return a / b
+
+div(2, 4)  # 2.0
+print(div(2, 4))  # 2.0
+
+# Using decorators you can add some additional functionality to the function
+# without changing the function itself.
+# Decorators are functions that wrap another function and add some additional
+
+def smart_div(func):
+    def inner(a,b):
+        if a<b:
+            a,b= b,a
+        return func(a,b)
+    return inner
+
+div = smart_div(div)
+
+print(div(2, 4))  # 2.0
