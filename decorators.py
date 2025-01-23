bf64d071b14cfcdf31e4c35059caf946a076a8cd
hrishikeshtawade04 <hrishikeshtawade04@gmail.com>
2025-01-22 19:35:53 -0800

    Added decorators and modules

--- decorators.py
+++ decorators.py
@@ -7,6 +7,7 @@ print(div(2, 4))  # 2.0
 # Using decorators you can add some additional functionality to the function
 # without changing the function itself.
 # Decorators are functions that wrap another function and add some additional
+# functionality to it.
 
 def smart_div(func):
     def inner(a,b):
---
+++ modules.py
@@ -0,0 +1,4 @@
+from calc import *
+
+print(add(1, 2))
+print(sub(1, 2))
