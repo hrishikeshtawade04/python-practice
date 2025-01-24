5ac0ea6a0b6bed69f2be519e58fb2d370e2ae7d2
hrishikeshtawade04 <hrishikeshtawade04@gmail.com>
2025-01-24 10:59:48 -0800

    Updated oops with constructir

--- oops.py
+++ oops.py
@@ -9,7 +9,7 @@ class Computer:
     def config(self):
         print("Hello config ", self.cpu, self.ram)
 
-
+# Created in heap memory
 com1 = Computer("i5", 16)
 print(type(com1))  # <class '__main__.Computer'>
 
